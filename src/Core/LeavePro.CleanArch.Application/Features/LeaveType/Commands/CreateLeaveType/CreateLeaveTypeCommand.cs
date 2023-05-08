b0565e594d58f7a9aff86d759f53199a1ce6a35c
mohsenpdev <mohsen.p.dev@gmail.com>
2023-05-08 12:33:31 +0430

    Corrected CreateLeaveTypeCommand

--- src/Core/LeavePro.CleanArch.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommand.cs
+++ src/Core/LeavePro.CleanArch.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommand.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
-using LeavePro.CleanArch.Application.Contracts.Persistence;
 using MediatR;
 
 namespace LeavePro.CleanArch.Application.Features.LeaveType.Commands.CreateLeaveType;
@@ -13,25 +11,4 @@ public class CreateLeaveTypeCommand : IRequest<int>
 {
     public string Name { get; set; } = string.Empty;
     public int DefaultDays { get; set; }
-}
-
-public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
-{
-    private readonly IMapper _mapper;
-    private readonly ILeaveTypeRepository _leaveTypeRepository;
-
-    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
-    {
-        _mapper = mapper;
-        _leaveTypeRepository = leaveTypeRepository;
-    }
-
-    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
-    {
-        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
-
-        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
-
-        return leaveTypeToCreate.Id;
-    }
 }
