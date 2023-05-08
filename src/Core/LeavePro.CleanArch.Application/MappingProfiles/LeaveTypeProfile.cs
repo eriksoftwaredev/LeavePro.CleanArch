04d998448fc95d3b7709bc27431675d716749a45
mohsenpdev <mohsen.p.dev@gmail.com>
2023-05-08 11:24:54 +0430

    Added automapper and mediatr

---
+++ src/Core/LeavePro.CleanArch.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LeavePro.CleanArch.Domain;
+
+namespace LeavePro.CleanArch.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+    }
+
+}
