1d3fa7f8763318c9382b4dbe783b848b2e5008cc
mohsenpdev <mohsen.p.dev@gmail.com>
2023-05-09 19:40:39 +0430

    Added LeavePro.CleanArch.Infrastructure project

---
+++ src/Infrastructure/LeavePro.CleanArch.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeavePro.CleanArch.Infrastructure;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+
+        return services;
+    }
+}
+
